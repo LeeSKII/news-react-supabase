b899868904c4eead086a74ebfad3aba094ceaee8
DESKTOP-LFEE3V4\admin <leeskiikseel@outlook.com>
2025-08-31 17:46:36 +0800

    add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
 
   return (
     <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
       <ul>
         {news.map((news_item) => (
           <li key={news_item.id}>{news_item.title}</li>
